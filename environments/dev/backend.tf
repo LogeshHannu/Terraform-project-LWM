69e7ba97de6a93dbaab896107fb682db2e139a09
LogeshHannuRaja <LogeshHannuRaja>
2026-09-24 12:34:52 +0530

    Added Jeniksfile and dev environment files

---
+++ environments/dev/backend.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.66.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket    = "lwmproject-0001"
+    key       = "environments/dev/terraform.tfstate" # place to store the state file in the bucket
+    region    = "us-east-1" # we cant pass variable here as the backend is initialized before the provider, so we have to hardcode the region here
+  }
+}
---
+++ environments/dev/main.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+  source = "../../modules/vpc"
+  vpc_cidr_block = "10.0.0.0/16"
+  subnet_cidr_block = "10.0.1.0/24"
+  environment_name = "dev"
+}
+
+module "ec2" {
+  source = "../../modules/ec2"
+  ami_id = "ami-0b6d9d3d33ba97d99"
+  instance_type = "t3.micro"
+  subnet_id = module.vpc.subnet_id
+  environment = "dev"
+  instance_name = "dev-instance"
+  instance_count = 1
+}
