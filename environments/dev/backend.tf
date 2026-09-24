5bba5d88c205eb15d19ce7a404fd2db32400f5ac
LogeshHannuRaja <LogeshHannuRaja>
2026-09-24 13:29:08 +0530

    updated backend file in dev

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -17,5 +17,7 @@ terraform {
     bucket    = "lwmproject-0001"
     key       = "environments/dev/terraform.tfstate" # place to store the state file in the bucket
     region    = "us-east-1" # we cant pass variable here as the backend is initialized before the provider, so we have to hardcode the region here
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
   }
 }
